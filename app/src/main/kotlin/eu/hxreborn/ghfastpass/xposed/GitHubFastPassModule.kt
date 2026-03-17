506b3bc20c86baad9d6596c20313fdcbbf5e1e70
hxreborn <hxreborn@duck.com>
2026-03-17 17:41:39 +0100

    feat!: migrate hooks to API 101 interceptor chain
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- app/src/main/kotlin/eu/hxreborn/ghfastpass/xposed/GitHubFastPassModule.kt
+++ app/src/main/kotlin/eu/hxreborn/ghfastpass/xposed/GitHubFastPassModule.kt
@@ -1,33 +1,31 @@
 package eu.hxreborn.ghfastpass.xposed
 
+import android.util.Log
 import eu.hxreborn.ghfastpass.BuildConfig
 import eu.hxreborn.ghfastpass.xposed.hook.TwoFactorHooker
-import io.github.libxposed.api.XposedInterface
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
-import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
+import io.github.libxposed.api.XposedModuleInterface.PackageReadyParam
 
-class GitHubFastPassModule(
-    base: XposedInterface,
-    param: ModuleLoadedParam,
-) : XposedModule(base, param) {
-    init {
-        log("GH FastPass v${BuildConfig.VERSION_NAME} loaded")
+class GitHubFastPassModule : XposedModule() {
+    override fun onModuleLoaded(param: ModuleLoadedParam) {
+        log(Log.INFO, TAG, "GH FastPass v${BuildConfig.VERSION_NAME} loaded")
     }
 
-    override fun onPackageLoaded(param: PackageLoadedParam) {
+    override fun onPackageReady(param: PackageReadyParam) {
         if (param.packageName != GITHUB_PACKAGE || !param.isFirstPackage) return
 
         runCatching {
             TwoFactorHooker.hook(this, param.classLoader)
         }.onSuccess {
-            log("Hooks registered for $GITHUB_PACKAGE")
+            log(Log.INFO, TAG, "Hooks registered for $GITHUB_PACKAGE")
         }.onFailure {
-            log("Hook registration failed", it)
+            log(Log.ERROR, TAG, "Hook registration failed", it)
         }
     }
 
     private companion object {
+        const val TAG = "GHFastPass"
         const val GITHUB_PACKAGE = "com.github.android"
     }
 }
